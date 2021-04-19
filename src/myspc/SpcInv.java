0f7e9925dc4245ee05dbb5627d3eae2c56fe3934
leapbtw <leonardoopaschetto@gmail.com>
2021-04-19 21:21:48 +0200

    0.0.5
    start menu added

--- src/myspc/SpcInv.java
+++ src/myspc/SpcInv.java
@@ -3,11 +3,12 @@ package myspc;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class SpcInv extends JFrame {
-	static boolean start = false;
     SoundPlayer sound = new SoundPlayer();
 	static SpcInv game = new SpcInv();
+	static boolean fix = false;
     
 	public SpcInv() {
         setSize(Commons.BOARD_WIDTH, Commons.BOARD_HEIGHT);
@@ -15,26 +16,30 @@ public class SpcInv extends JFrame {
         setResizable(false);
         setLocationRelativeTo(null);
 		addMouseListener(new MAdapter());
-		//(new Soundtrack()).run();
 	}
 	
 	public static void main(String[]args) {
 		StartMenu sm = new StartMenu();
-		Board bd = new Board();
 		game.add(new StartMenu());
 		game.setVisible(true);
+		(new Soundtrack()).run();
 	}
 	
 	private class MAdapter extends MouseAdapter {
 		@Override
 		public void mousePressed(MouseEvent e) {
-			int key = e.getButton();
-			int mx = e.getXOnScreen();
-			int my = e.getYOnScreen();
-			if (key == 1 && ((mx >= 875 && mx <= 1065) && (my >= 755 && my <= 835))) {
-				System.out.println("prega che parta");
-				start = true;
+			int mx = 0, my = 0, key = e.getButton();
+			String mb = e.getPoint().toString();
+			try {
+				mx = Integer.parseInt(mb.substring(17, 20));
+				my = Integer.parseInt(mb.substring(23, 26));
+			} catch (NumberFormatException exc) {} // try-catch per problemi col substring(), ad ogni modo è sotto controllo
+			
+			if (SpcInv.fix == false && key == 1 && ((mx >= 250 && mx <= 475) && (my >= 550 && my <= 650))) {
+				SpcInv.fix = true;
 				game.dispose();
+				game = new SpcInv();
+				Board bd = new Board();
 				game.add(new Board());
 				game.setVisible(true);
 			}
@@ -43,8 +48,6 @@ public class SpcInv extends JFrame {
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			int key = e.getButton();
-			int mx = e.getXOnScreen();
-			int my = e.getYOnScreen();
 		}
 	}
 }
--- src/myspc/obj/Player.java
+++ src/myspc/obj/Player.java
@@ -16,7 +16,7 @@ public class Player extends Sprite {
 	
 	private void initPlayer() {
 		
-		var playerImg = "src/images/player.png";
+		var playerImg = "src\\images\\player.png";
 		var ii = new ImageIcon(playerImg);
 		
 		width = ii.getImage().getWidth(null);
@@ -30,20 +30,20 @@ public class Player extends Sprite {
 	}
 	
 	public void move() {
-		x+=dx; //coord + mov
+		x += dx; //coord + mov
 		if (x <= 2) x = 2;  //bordo sinistro
 		if (x >= Commons.BOARD_WIDTH - 2 * width) x = Commons.BOARD_WIDTH - 2 * width;  //bordo destro
 	}
 	
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		if(key==KeyEvent.VK_LEFT || key == KeyEvent.VK_A) dx = -4;//-2
-		if(key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) dx = 4;//2
+		if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) dx = -4;//-2
+		if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) dx = 4;//2
 	}
         
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
-		if(key==KeyEvent.VK_LEFT || key == KeyEvent.VK_A) dx = 0;	
-		if(key==KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) dx = 0;
+		if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_A) dx = 0;	
+		if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_D) dx = 0;
     }
 }
